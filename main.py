b5e257cbffcfb586d225db5d651a9876bbea86ef
wolfshctorm <dsshir2011@gmail.com>
2025-05-05 19:02:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from ast import literal_eval
 from distutils.command.clean import clean
 
 
-class Drob:
+class Fraction:
     def __init__(self, value1, value2):
         self.value1 = value1
         self.value2 = value2
@@ -19,82 +20,90 @@ class Drob:
         return self.value1-self.value2
 
     def get_list(self):
-        return [self.value1 * self.value2, int(self.value1 / self.value2), int(self.value1 - self.value2), int(self.value1 + self.value2)]
+        return (f'Результат умножения: {self.value1 * self.value2}\nРезультат деления: {int(self.value1 / self.value2)}\nРезультат вычитания: {int(self.value1 - self.value2)}\nРезультат сложения: {int(self.value1 + self.value2)}')
 
-num_class = Drob(4/8, 1/2)
-
-print(num_class.get_list())
-
-
-
-# value1 = celcia
-# value2 = farengeit
+# value3 = celcia
+# value4 = farengeit
 class Graducnic:
-    def __init__(self, value1, value2):
-        self.value1 = value1
-        self.value2 = value2
+    def __init__(self, value3, value4):
+        self.value3 = value3
+        self.value4 = value4
 
     @staticmethod
     def celtofar(self):
-        return (self.value1*9/5)+32
+        return (self.value3*9/5)+32
 
     @staticmethod
     def fartocel(self):
-        return (self.value2-32)*5/9
-
-
+        return (self.value4-32)*5/9
 
     def get_list(self):
-        return [(self.value1*9/5)+32, (self.value2-32)*5/9]
+        return (f'Цельсия в фаренгейты: {(self.value3*9/5)+32}\nФаренгейты в цельсия: {(self.value4-32)*5/9}')
 
-num_class = Graducnic(0,  212)
-
-print(num_class.get_list())
-
-# value1 = metr
-# value2 = mili
+# value5 = metr
+# value6 = mili
 #Возможна погрешность
 class Perevod:
-    def __init__(self, value1, value2):
-        self.value1 = value1
-        self.value2 = value2
+    def __init__(self, value5, value6):
+        self.value5 = value5
+        self.value6 = value6
 
     @staticmethod
     def metrtomili(self):
-        return self.value2/1609
+        return self.value6/1609
 
     @staticmethod
     def militometr(self):
-        return self.value1*1609
+        return self.value5*1609
 
     def get_list(self):
-        return [self.value2/1609, self.value1*1609]
+        return (f'Метры в мили: {self.value6 / 1609}\nМили в метры: {self.value5 * 1609}')
 
-
-num_class = Perevod(10, 212)
-
-print(num_class.get_list())
-
-# value1 = litr
-# value2 = galon
+# value7 = litr
+# value8 = galon
 #Возможна погрешность
 class Perevod2:
-    def __init__(self, value1, value2):
-        self.value1 = value1
-        self.value2 = value2
+    def __init__(self, value7, value8):
+        self.value7 = value7
+        self.value8 = value8
 
     @staticmethod
     def litrtogalon(self):
-        return self.value1/3.785
+        return self.value7/3.785
 
     @staticmethod
     def galontolitr(self):
-        return self.value2*3.785
+        return self.value8*3.785
     def get_list(self):
-        return [self.value1/3.785, self.value2*3.785]
+        return (f'Литры в галоны: {self.value7/3.785}\nГалоны в литры: {self.value8*3.785}')
 
-
-num_class = Perevod2(10, 10)
-
-print(num_class.get_list())
+# Интерфейс
+print('Выберете действие:\n1-Работа с дробями\n2-Перевод из цельсий в фаренгейты и наоборот\n3-Перевод из миль в метры и наоборот\n4-Перевод из галонов в литры и наоборот')
+action_choice = input('Ваш выбор(напишите цифру):')
+if '1' in action_choice:
+    print('Вы выбрали работа с дробями!\nВведите данные:')
+    fraction1 = int(input('Первая дробь:'))
+    fraction2 = int(input('Вторая дробь:'))
+    num_class = Fraction(fraction1, fraction2)
+    print(num_class.get_list())
+if '2' in action_choice:
+    print('Вы выбрали перевод из цельсий в фаренгейты и наоборот!\nВведите данные:')
+    fahrenheit = int(input('Фаренгейты:'))
+    celsius = int(input('Цельсия:'))
+    num_class = Graducnic(celsius, fahrenheit)
+    print(num_class.get_list())
+if '3' in action_choice:
+    print('Перевод из миль в метры и наоборот!\nВведите данные:')
+    mili = int(input('Мили:'))
+    metr = int(input('Метры:'))
+    num_class = Perevod(metr, mili)
+    print(num_class.get_list())
+    print('!!!Может присутствовать небольшая погрешность!!!')
+if '4' in action_choice:
+    print('Перевод из галонов в литры и наоборот!\nВведите данные:')
+    litr = int(input('Литры:'))
+    galon = int(input('Галоны:'))
+    num_class = Perevod2(litr, galon)
+    print(num_class.get_list())
+    print('!!!Может присутствовать небольшая погрешность!!!')
 
